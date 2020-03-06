f3f8c224198cdfed0437087575169c23221d247b
LUCAS WENDEL COSTA DOS SANTOS <1-2020117892@ACADEMICO.LOCAL>
2020-03-05 21:27:12 -0300

    final aula

--- lp20200227/src/lp20200305/Maca.java
+++ lp20200227/src/lp20200305/Maca.java
@@ -4,6 +4,7 @@ import java.text.NumberFormat;
 import java.util.Locale;
 import java.util.Scanner;
 import static utils.Utils.*;
+import java.util.ArrayList;
 
 public class Maca {
 	public static Scanner leia = new Scanner(System.in);
@@ -14,17 +15,34 @@ public class Maca {
 		
 		final double  MACA_DUZIA = 1,
 					  MACA_NORMAL = 1.30;
-			  double total,
-				     qtdMaca;
-		
-		System.out.print("Digite a qtd de maças compradas: ");
+			  double total = 0,
+				     qtdMaca,
+				     opc;
+			  ArrayList<String> itens = new ArrayList<String>();
+			  itens.add("Maçã");
+			  itens.add("Uva");
+			  
+		opc = menu(itens);
+		 
+		System.out.print("Digite a qtd: ");
 		qtdMaca = leia.nextDouble();
-	
-		if(qtdMaca < 12) {
+		
+		switch ((int)opc) {
+			case 1:
+				total = qtdMaca * 1.60;
+				break;
+			case 2:
+				total = qtdMaca * 5.50;
+				break;
+			default:
+			break;
+		}
+		/*if(qtdMaca < 12) {
 			total = qtdMaca * MACA_NORMAL;
 		}else {
 			total = qtdMaca * MACA_DUZIA;
-		}
+		}*/
+		
 		
 		System.out.println("Valor a ser pago: " + format.format(total));
 		
--- lp20200227/src/utils/Utils.java
+++ lp20200227/src/utils/Utils.java
@@ -1,9 +1,23 @@
 package utils;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class Utils {
 	public static void header(String text) {
 		String equals = "=".repeat(10);
 		System.out.println(equals + text + equals);
 		System.out.println("");
+	}
+
+	public static int menu(List<String> itens) {
+		Scanner leia = new Scanner(System.in);
+		for (String item : itens) {
+			System.out.println("[" + (itens.indexOf(item) + 1) + "] - " + item);
+		}
+		System.out.print("Escolha uma opção: ");
+		int opc = leia.nextInt();
+		return opc;
+
 	}
 }
